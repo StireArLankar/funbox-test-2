480faed119b6f41a858acbc5d6ac34babc82568d
StireArLankar <stire.ru@gmail.com>
2019-02-24 22:27:08 +0300

    Updates

---
+++ precache-manifest.8a5b974e9979fed3042cb7135ce26b4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e52963b3da53311cf74c",
+    "url": "/funbox-test-2/static/js/runtime~main.e52963b3.js"
+  },
+  {
+    "revision": "bb44904c6eff14e05b2a",
+    "url": "/funbox-test-2/static/js/main.bb44904c.chunk.js"
+  },
+  {
+    "revision": "b1044ed6bd902fe3b5a0",
+    "url": "/funbox-test-2/static/js/2.b1044ed6.chunk.js"
+  },
+  {
+    "revision": "bb44904c6eff14e05b2a",
+    "url": "/funbox-test-2/static/css/main.99c2e365.chunk.css"
+  },
+  {
+    "revision": "9e88aadf77b43ef168062238e997fdc8",
+    "url": "/funbox-test-2/index.html"
+  }
+];
